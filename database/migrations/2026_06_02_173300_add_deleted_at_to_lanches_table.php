436fac3a565881c94edb40f7e3c9b338a9b0d296
arrudoprogramador <joaopedroarruda323@gmail.com>
2026-06-02 14:46:30 -0300

    agora vai 2.0

--- database/migrations/2026_06_02_173300_add_deleted_at_to_lanches_table.php
+++ database/migrations/2026_06_02_173300_add_deleted_at_to_lanches_table.php
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('lanches', function (Blueprint $table) {
-            //
+            $table->dropSoftDeletes();
         });
     }
 };
